339953a5145535d3912dc380a1a55c986799ce50
Adil cp <adilcp8590@gmailcom>
2025-03-26 14:47:44 +0530

    following mvvm architecture

---
+++ lib/models/todo.dart
@@ -0,0 +1,37 @@
+class Todo {
+  String id;
+  String title;
+  String description;
+  DateTime dueDate;
+  bool isCompleted;
+
+  Todo({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.dueDate,
+    this.isCompleted = false,
+  });
+
+  // Convert Todo object to Firestore JSON format
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'dueDate': dueDate.toIso8601String(), // Convert DateTime to string
+      'isCompleted': isCompleted,
+    };
+  }
+
+  // Create a Todo object from Firestore document snapshot
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      dueDate: DateTime.parse(json['dueDate']), // Convert string to DateTime
+      isCompleted: json['isCompleted'] ?? false,
+    );
+  }
+}
